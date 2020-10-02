d3c5e9e0e0902dd3c354079de776c705cb2229c9
Abdellah benchellha <abdellahhs97@gmail.com>
2020-10-02 17:02:45 +0100

    delete Chart add design all page

--- routes/web.php
+++ routes/web.php
@@ -145,3 +145,8 @@ Route::get('offres/edite', function (){ return view('offres.edite'); })->name('o
 Route::get('users', function (){ return view('users.index'); })->name('users.index');
 Route::get('users/create', function (){ return view('users.create'); })->name('users.create');
 Route::get('users/edite', function (){ return view('users.edite'); })->name('users.edite');
+
+/* Prouit */
+Route::get('produit', function (){ return view('produit.index'); })->name('produit.index');
+Route::get('produit/create', function (){ return view('produit.create'); })->name('produit.create');
+Route::get('produit/edite', function (){ return view('produit.edite'); })->name('produit.edite');
